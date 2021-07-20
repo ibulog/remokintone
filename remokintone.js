a897c8d22414e26e98c47bf90c3e54b2748c4814
Ibuki Yamashita <charo.little@gmail.com>
2021-07-20 06:35:43 +0000

    initial commit

---
+++ remokintone.js
@@ -0,0 +1,66 @@
+'use strict'
+
+const FormData = require('form-data');
+const { URL, URLSearchParams } = require('url');
+const fetch = require('node-fetch');
+const Headers = fetch.Headers;
+
+const deviceUrl = 'https://api.nature.global/1/devices';
+const applianceUrl = 'https://api.nature.global/1/appliances';
+
+const getData = async (url, param) => {
+    const resp = await fetch(url, param);
+    const data = await resp.json();
+
+    return data;
+}
+
+(async () => {
+    const param = {
+        method: 'GET',
+        headers: {
+            'Authorization': RTOKEN
+        }
+    };
+
+    const deviceData = await getData(deviceUrl, param);
+    const applianceData = await getData(applianceUrl, param);
+    
+    const body = {
+        'app': APPID,
+        'record': {
+            'temp': {
+                'value': deviceData[0].newest_events.te.val 
+            },
+            'target_temp': {
+                'value': applianceData[0].settings.temp
+            },
+            'mode': {
+                'value': applianceData[0].settings.mode 
+            },
+            'vol': {
+                'value': applianceData[0].settings.vol 
+            },
+            'dir': {
+                'value': applianceData[0].settings.dir 
+            }
+        }
+    };
+  
+    const resp = await fetch(KINTONEURL, {
+        method: 'POST',
+        headers: {
+            'X-Cybozu-API-Token': KTOKEN,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    });
+
+    if(resp.ok) {
+        const result = await resp.json();
+        console.log(result);
+    } else {
+        const error = await resp.json();
+        console.log(error);
+    }
+})();
